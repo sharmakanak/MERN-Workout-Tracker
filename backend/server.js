784a5f04db4d50185603d73781221a3851548e42
{kanak} <sharmakanak2108@gmail.com>
2026-07-30 10:35:40 +0530

    Backend API CRUD Completed

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 require("dotenv").config()
 
 const express = require("express")
+
 const mongoose = require("mongoose")
 const app = express()
 const workoutRoutes = require("./routes/workouts")
 
-app.use("/api/workouts", workoutRoutes)
 app.use(express.json())
+app.use("/api/workouts", workoutRoutes)
+
 
 app.get("/", (req, res) => {
     res.json({ message: "Workout Tracker API" })
