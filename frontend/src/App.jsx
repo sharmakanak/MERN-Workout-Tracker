474c6ed783e6e87bc27e10adf4047af359739904
{kanak} <sharmakanak2108@gmail.com>
2026-07-31 12:27:54 +0530

    Lesson 2: Complete React Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,37 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+
+// pages & components
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
+  const { user } = useAuthContext();
+
   return (
-    <div>
-      <h1>Workout Tracker</h1>
+    <div className="App">
+      <Navbar />
+
+      <div className="pages">
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/signup"
+            element={!user ? <Signup /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
